5edf0d46102ee109634cbc31525f00ac848e07a8
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-07-15 19:45:55 -0700

    Add vehicle tilt

--- Car_rb.gd
+++ Car_rb.gd
@@ -1,14 +1,19 @@
 extends RigidBody3D
 
 var speed := 80
-var boost_speed := 160
+var boost_speed := 120
 var steering_factor := 2.0
+var steering_tilt := 0.5
 
 var gravity_force := 50.0
 var local_gravity := Vector3.DOWN
 var rotation_speed := 5.0
 
+
+var is_grounded := false 
+
 @onready var ray : RayCast3D = %RayCast3D
+@onready var body : MeshInstance3D = %CarBody
 
 func _ready() -> void:
 	pass
@@ -19,17 +24,31 @@ func _physics_process(delta: float) -> void:
 func _integrate_forces(state):
 	pass
 
-func apply_movement(delta):
+func apply_movement(delta: float):
 	var input_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	
 	var movement_velocity = Vector3(0, 0, input_vector.y).normalized() * speed
 	transform.basis = Basis(Vector3.UP, -steering_factor * input_vector.x * delta) * transform.basis
+	tilt_body(delta,input_vector)
 	
 	apply_central_force(basis * movement_velocity)
 	apply_central_force(basis * Vector3.DOWN * gravity_force)
 
 func bounce():
 	pass
+
+func tilt_body( delta: float, input_vector: Vector2):
+	var home_tilt := Quaternion()
+	var desired_tilt := Quaternion(Vector3(0,0,1), -input_vector.x * steering_tilt)
+	
+	var target_rotation = desired_tilt
+	var slerp_speed = 5.0
+	
+	var current_rot = Quaternion(body.transform.basis)
+	var new_rot = current_rot.slerp(target_rotation, delta* slerp_speed)
+	body.transform.basis = Basis(new_rot)
+	
+	
 
 func orient_car(direction: Vector3, delta: float) -> void: 
 	var left_axis := -local_gravity.cross(direction)
--- RayCast3D.gd
+++ RayCast3D.gd
@@ -4,7 +4,7 @@ extends RayCast3D
 
 @export var ride_height := -9.0
 @export var ride_spring_damper := 10.0
-@export var ride_spring_strength := 50.0
+@export var ride_spring_strength := 100.0
 
 
 
@@ -22,39 +22,18 @@ func apply_spring_force(delta):
 	var vel = car.linear_velocity 
 	var ray_dir := global_basis.y
 	
-	var other_vel := Vector3.ZERO
-	
 	var ray_dir_vel = vel.dot(ray_dir)
-	
 	var rel_vel = ray_dir_vel 
 	
 	var distance = -get_collision_point().distance_to(global_position)
 	var x = distance - ride_height
 	
 	var spring_force = (x * ride_spring_strength) - (rel_vel * ride_spring_damper)
-	
 	car.apply_central_force(ray_dir * spring_force)
 
 func align_to_normal(delta):
 	var normal := get_collision_normal()
 	
-	print(normal)
+	#print(normal)
 	
-	var target_up = get_collision_normal()
-	var current_up = global_transform.basis.y
-	var rotation_to_target = current_up.cross(target_up).normalized()
-	var angle_to_target = acos(current_up.dot(target_up))
-	var rotation_delta = rotation_to_target * angle_to_target * delta * 10
-	car.angular_velocity = car.angular_velocity.lerp(rotation_delta, 0.1)
-	#var rotation_axis = Vector3.UP.cross(normal).normalized()
-	#var rotation_angle = Vector3.UP.angle_to(normal)
-	#car.transform.basis = Basis(rotation_axis, rotation_angle)
-
-	'''
-	var forward = transform.basis.z
-	var right = normal.cross(forward).normalized()
-	var up = -right.cross(forward).normalized()
-	car.transform.basis = Basis(right, up, forward)
-	
-	print(car.transform.basis, Basis(right, up, forward))'''
 
